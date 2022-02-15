1bb77102bff7357400f54ad7621e22fdd4bd7311
vladigris <vladigris@gmail.com>
2022-02-16 02:10:38 +0300

    Next

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
+// authenticator
+
+$(document).on('click', '#authenticator-git-modal-link', function () {
+    $('#authenticator-git-modal').addClass('is-active')
+})
+
+$(document).on('click', '#authenticator-git-modal .modal-close', function () {
+    $('#authenticator-git-modal').removeClass('is-active')
+})
+
+$(document).on('click', '#authenticator-loc-modal-link', function () {
+    $('#authenticator-loc-modal').addClass('is-active')
+})
+
+$(document).on('click', '#authenticator-loc-modal .modal-close', function () {
+    $('#authenticator-loc-modal').removeClass('is-active')
+})
+
+// lincrew
+
+$(document).on('click', '#lincrew-git-modal-link', function () {
+    $('#lincrew-git-modal').addClass('is-active')
+})
+
+$(document).on('click', '#lincrew-git-modal .modal-close', function () {
+    $('#lincrew-git-modal').removeClass('is-active')
+})
+
+$(document).on('click', '#lincrew-loc-modal-link', function () {
+    $('#lincrew-loc-modal').addClass('is-active')
+})
+
+$(document).on('click', '#lincrew-loc-modal .modal-close', function () {
+    $('#lincrew-loc-modal').removeClass('is-active')
+})
+
+// pusher
+
+$(document).on('click', '#pusher-git-modal-link', function () {
+    $('#pusher-git-modal').addClass('is-active')
+})
+
+$(document).on('click', '#pusher-git-modal .modal-close', function () {
+    $('#pusher-git-modal').removeClass('is-active')
+})
+
+$(document).on('click', '#pusher-loc-modal-link', function () {
+    $('#pusher-loc-modal').addClass('is-active')
+})
+
+$(document).on('click', '#pusher-loc-modal .modal-close', function () {
+    $('#pusher-loc-modal').removeClass('is-active')
+})
+
 // qaryb
 
 $(document).on('click', '#qaryb-git-modal-link', function () {
-    console.log('hello')
     $('#qaryb-git-modal').addClass('is-active')
 })
 
@@ -10,10 +63,63 @@ $(document).on('click', '#qaryb-git-modal .modal-close', function () {
 })
 
 $(document).on('click', '#qaryb-loc-modal-link', function () {
-    console.log('hello')
     $('#qaryb-loc-modal').addClass('is-active')
 })
 
 $(document).on('click', '#qaryb-loc-modal .modal-close', function () {
     $('#qaryb-loc-modal').removeClass('is-active')
+})
+
+// tabletop
+
+$(document).on('click', '#tabletop-git-modal-link', function () {
+    $('#tabletop-git-modal').addClass('is-active')
+})
+
+$(document).on('click', '#tabletop-git-modal .modal-close', function () {
+    $('#tabletop-git-modal').removeClass('is-active')
+})
+
+$(document).on('click', '#tabletop-loc-modal-link', function () {
+    $('#tabletop-loc-modal').addClass('is-active')
+})
+
+$(document).on('click', '#tabletop-loc-modal .modal-close', function () {
+    $('#tabletop-loc-modal').removeClass('is-active')
+})
+
+// tethree
+
+$(document).on('click', '#tethree-git-modal-link', function () {
+    $('#tethree-git-modal').addClass('is-active')
+})
+
+$(document).on('click', '#tethree-git-modal .modal-close', function () {
+    $('#tethree-git-modal').removeClass('is-active')
+})
+
+$(document).on('click', '#tethree-loc-modal-link', function () {
+    $('#tethree-loc-modal').addClass('is-active')
+})
+
+$(document).on('click', '#tethree-loc-modal .modal-close', function () {
+    $('#tethree-loc-modal').removeClass('is-active')
+})
+
+// vpnmaker
+
+$(document).on('click', '#vpnmaker-git-modal-link', function () {
+    $('#vpnmaker-git-modal').addClass('is-active')
+})
+
+$(document).on('click', '#vpnmaker-git-modal .modal-close', function () {
+    $('#vpnmaker-git-modal').removeClass('is-active')
+})
+
+$(document).on('click', '#vpnmaker-loc-modal-link', function () {
+    $('#vpnmaker-loc-modal').addClass('is-active')
+})
+
+$(document).on('click', '#vpnmaker-loc-modal .modal-close', function () {
+    $('#vpnmaker-loc-modal').removeClass('is-active')
 })
